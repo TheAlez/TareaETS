c1bc7b800e6808bb6883cb73e4461500571aed94
Nuhazet <Nuhazet@DESKTOP-7O52PBJ>
2026-03-05 01:23:22 +0000

    a ver si funciona

--- src/tareaets/TareaETS.java
+++ src/tareaets/TareaETS.java
@@ -16,7 +16,8 @@ public class TareaETS {
     public static void main(String[] args) {
         // TODO code application logic here
         int nunu=67;
-        
+        int nini=69;
+        //porfavor
         
         // keloke aki juanjo 
          int espaciosBlancos = 0;
